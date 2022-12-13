23e6949fcc8e5453520c3f58c0cb36590ae56ecc
I1ss <lebedewgeorg@yandex.ru>
2022-12-14 01:15:23 +0700

    UR-1 - Добавлены процессы авторизации/регистрации в приложении.

---
+++ UpRestaraunt/UpRestaraunt/Commands/BaseTypeCommand.cs
@@ -0,0 +1,60 @@
+﻿namespace UpRestaraunt.Commands
+{
+    using System;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Базовый класс команды.
+    /// </summary>
+    /// <typeparam name="T"> Тип принимаемой вью-модели. </typeparam>
+    public abstract class BaseTypeCommand<T> : ICommand
+    {
+        /// <summary>
+        /// Событие выполнения.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// Выполнение команды.
+        /// </summary>
+        /// <param name="parameter"> Параметр команды. </param>
+        protected abstract void Execute(T parameter);
+
+        /// <summary>
+        /// Выполнение команды.
+        /// </summary>
+        /// <param name="parameter"> Параметр команды. </param>
+        public virtual void Execute(object parameter)
+        {
+            if (parameter is T typedParameter)
+                Execute(typedParameter);
+        }
+
+        /// <summary>
+        /// Проверка возможности ли вызвать команду.
+        /// </summary>
+        /// <param name="parameter"> Параметр для команды. </param>
+        /// <returns> Возможно ли? </returns>
+        protected virtual bool CanExecute(T parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка возможности ли вызвать команду.
+        /// </summary>
+        /// <param name="parameter"> Параметр для команды. </param>
+        /// <returns> Возможно ли? </returns>
+        public virtual bool CanExecute(object parameter)
+        {
+            if (parameter is T typedParameter)
+                return CanExecute(typedParameter);
+
+            return true;
+        }
+    }
+}
---
+++ UpRestaraunt/UpRestaraunt/Converters/VisibleOrHiddenConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace UpRestaraunt.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Конвертер, определяющий, отображать или скрывать объект.
+    /// </summary>
+    public class VisibleOrHiddenConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is IConvertible conv && conv.ToInt64(null) != 0 ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
